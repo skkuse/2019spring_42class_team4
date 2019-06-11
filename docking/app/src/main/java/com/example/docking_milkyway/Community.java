da0e271b473d011990e110639b3cc66136d7402e
eunha <1002galaxy@gmail.com>
2019-06-12 03:59:55 +0900

    firebase contents 구조 변경 및 그에 따른 쿼리문 변경

--- docking/app/src/main/java/com/example/docking_milkyway/Community.java
+++ docking/app/src/main/java/com/example/docking_milkyway/Community.java
@@ -48,7 +48,7 @@ public class Community extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.community, container, false);
 
-        final FirebaseFirestore contentsDB = FirebaseFirestore.getInstance();
+        final FirebaseFirestore fireDB = FirebaseFirestore.getInstance();
 
         ArrayList<ContentDB> recyclerlist = new ArrayList<>();
         Context context = view.getContext();
@@ -92,84 +92,58 @@ public class Community extends Fragment {
             mRecyclerView.setVisibility(View.VISIBLE);
             isnotlogin.setVisibility(View.GONE);
             uploading.setVisibility(View.VISIBLE);
-            contentsDB.collection("Contents").document(userid).get().addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    //success
-                    DocumentSnapshot document = task.getResult();
-                    if(document.exists()){
-                        Log.d("은하", "사용자 아이디에 해당하는 게시물 있음");
-                        Log.d("은하", "document : "+document.getData());
 
-                        ///ArrayList<ContentDB> contentsdata = new ArrayList<>();
-                        long contentssize = (long) document.getData().get("contentssize");
-                        Log.d("은하", "contentssize : "+contentssize);
-                        //int contentssize=2;
-                        for(int i=0 ; i < contentssize ; i++){
-                            DocumentReference contentsRef = contentsDB.collection("Contents")
-                                    .document(userid).collection(userid).document(String.valueOf(i));
-                            int finalI = i;
-                            Log.d("은하", finalI +": finalI");
-                            contentsRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                    if(task.isSuccessful()){
-                                        DocumentSnapshot contentdocument = task.getResult();
-                                        if(contentdocument.exists()){
-                                            Log.d("은하", contentdocument.toString());
-                                            ContentDB data = (ContentDB) contentdocument.toObject(ContentDB.class);
-                                            Log.d("은하", "data: "+data.toString());
-                                            recyclerlist.add(data);
-
-                                            Log.d("은하", "contentSSN : "+finalI + ", userid : "+userid);
-                                            ArrayList<CommentDB> commentslist = new ArrayList<>();
-                                            contentsDB.collection("Comments")
-                                                    .whereEqualTo("User_SSN", userid)
-                                                    .whereEqualTo("C_Num", finalI)
-                                                    .get()
-                                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                                        @Override
-                                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                            if (task.isSuccessful()) {
-                                                                Log.d("은하", "여기까지?commenttasksuccess");
-                                                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                                                    Log.d("은하", document.getId() + " => " + document.getData());
-                                                                    CommentDB tempcommentdb = document.toObject(CommentDB.class);
-                                                                    commentslist.add(tempcommentdb);
-                                                                    Log.d("은하", "commentslist에 잘 추가되었나? "+commentslist);
-                                                                }
-                                                                Log.d("은하", "finalI : "+ finalI +", commentslist : "+commentslist);
-                                                                if(commentslist != null) {
-                                                                    commentDBS.add(finalI, commentslist);
-                                                                }
-                                                                Log.d("은하", "commentDBS : "+commentDBS.toString());
-                                                            } else {
-                                                                Log.d("은하", "Error getting documents: ", task.getException());
-                                                            }
-                                                            //getcommentsfirestore(finalI, userid);
-                                                            Log.d("은하", "여기 recyclerlist: "+recyclerlist);
-                                                            if(finalI == contentssize-1){
-                                                                Log.d("은하", "여기?");
-                                                                setrecyclerview(recyclerlist, context);
-                                                            }
+            fireDB.collection("Contents")
+                    .whereEqualTo("userSSN", userid)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()) {
+                                for(QueryDocumentSnapshot contentdocument : task.getResult()) {
+                                    Log.d("은하", contentdocument.getId() + " => " + contentdocument.getData());
+                                    ContentDB data = (ContentDB) contentdocument.toObject(ContentDB.class);
+                                    Log.d("은하", "data: "+data.toString());
+                                    recyclerlist.add(data);
+                                    Log.d("은하", "contentSSN : "+data.SSN + ", userid : "+userid);
+                                    ArrayList<CommentDB> commentslist = new ArrayList<>();
+                                    fireDB.collection("Comments")
+                                            .whereEqualTo("User_SSN", userid)
+                                            .whereEqualTo("C_Num", data.SSN)
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        Log.d("은하", "여기까지?commenttasksuccess");
+                                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                                            Log.d("은하", document.getId() + " => " + document.getData());
+                                                            CommentDB tempcommentdb = document.toObject(CommentDB.class);
+                                                            commentslist.add(tempcommentdb);
+                                                            Log.d("은하", "commentslist에 잘 추가되었나? "+commentslist);
                                                         }
-                                                    });
-                                        }
-                                    }else{
-                                        Log.d("은하", "get() failed");
-                                    }
-                                }
-                            });
-                        }
-                    }
-                    else{
-                        Log.d("은하", "사용자 아이디에 해당하는 게시물 없음");
-                    }
-                }
-                else{
-                    Log.d("은하", "get() failed");
-                }
+                                                        Log.d("은하", "finalI : "+ data.SSN +", commentslist : "+commentslist);
+                                                        if(commentslist != null) {
+                                                            commentDBS.add(data.SSN, commentslist);
+                                                        }
+                                                        Log.d("은하", "commentDBS : "+commentDBS.toString());
+                                                    } else {
+                                                        Log.d("은하", "Error getting documents: ", task.getException());
+                                                    }
+                                                    //getcommentsfirestore(finalI, userid);
+                                                    Log.d("은하", "여기 recyclerlist: "+recyclerlist);
+                                                    Log.d("은하", "여기?");
+                                                    setrecyclerview(recyclerlist, context);
 
-            });
+                                                }
+                                            });
+                                }
+                                }
+                            else{
+                                Log.d("은하", "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
 
         }
 
--- docking/app/src/main/java/com/example/docking_milkyway/Mypage.java
+++ docking/app/src/main/java/com/example/docking_milkyway/Mypage.java
@@ -41,93 +41,65 @@ public class Mypage extends AppCompatActivity {
 
         String userid = login_hist.getUserName(getApplicationContext());
 
-        FirebaseFirestore contentsDB = FirebaseFirestore.getInstance();
+        FirebaseFirestore fireDB = FirebaseFirestore.getInstance();
         ArrayList<ContentDB> recyclerlist = new ArrayList<>();
         Context context = Mypage.this;
 
 
         if(userid != null) {
 
-            contentsDB.collection("Contents").document(userid).get().addOnCompleteListener(task -> {
-                if (task.isSuccessful()) {
-                    //success
-                    DocumentSnapshot document = task.getResult();
-                    if(document.exists()){
-                        Log.d("은하", "사용자 아이디에 해당하는 게시물 있음");
-                        Log.d("은하", "document : "+document.getData());
-
-                        ///ArrayList<ContentDB> contentsdata = new ArrayList<>();
-                        long contentssize = (long) document.getData().get("contentssize");
-                        Log.d("은하", "contentssize : "+contentssize);
-                        //int contentssize=2;
-                        for(int i=0 ; i < contentssize ; i++){
-                            DocumentReference contentsRef = contentsDB.collection("Contents")
-                                    .document(userid).collection(userid).document(String.valueOf(i));
-                            int finalI = i;
-                            Log.d("은하", finalI +": finalI");
-                            contentsRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                    if(task.isSuccessful()){
-                                        DocumentSnapshot contentdocument = task.getResult();
-                                        if(contentdocument.exists()){
-                                            Log.d("은하", contentdocument.toString());
-                                            ContentDB data = (ContentDB) contentdocument.toObject(ContentDB.class);
-                                            Log.d("은하", "data: "+data.toString());
-                                            recyclerlist.add(data);
-                                            Log.d("은하", "contentSSN : "+finalI + ", userid : "+userid);
-                                            ArrayList<CommentDB> commentslist = new ArrayList<>();
-                                            contentsDB.collection("Comments")
-                                                    .whereEqualTo("User_SSN", userid)
-                                                    .whereEqualTo("C_Num", finalI)
-                                                    .get()
-                                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                                        @Override
-                                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                            if (task.isSuccessful()) {
-                                                                Log.d("은하", "여기까지?commenttasksuccess");
-                                                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                                                    Log.d("은하", document.getId() + " => " + document.getData());
-                                                                    CommentDB tempcommentdb = document.toObject(CommentDB.class);
-                                                                    commentslist.add(tempcommentdb);
-                                                                    Log.d("은하", "commentslist에 잘 추가되었나? "+commentslist);
-                                                                }
-                                                                Log.d("은하", "finalI : "+ finalI +", commentslist : "+commentslist);
-                                                                if(commentslist != null) {
-                                                                    commentDBS.add(finalI, commentslist);
-                                                                }
-                                                                Log.d("은하", "commentDBS : "+commentDBS.toString());
-                                                            } else {
-                                                                Log.d("은하", "Error getting documents: ", task.getException());
-                                                            }
-                                                            //getcommentsfirestore(finalI, userid);
-                                                            Log.d("은하", "여기 recyclerlist: "+recyclerlist);
-                                                            if(finalI == contentssize-1){
-                                                                Log.d("은하", "여기?");
-                                                                setrecyclerview(recyclerlist, context);
-                                                            }
+            fireDB.collection("Contents")
+                    .whereEqualTo("userSSN", userid)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()) {
+                                for(QueryDocumentSnapshot contentdocument : task.getResult()) {
+                                    Log.d("은하", contentdocument.getId() + " => " + contentdocument.getData());
+                                    ContentDB data = (ContentDB) contentdocument.toObject(ContentDB.class);
+                                    Log.d("은하", "data: "+data.toString());
+                                    recyclerlist.add(data);
+                                    Log.d("은하", "contentSSN : "+data.SSN + ", userid : "+userid);
+                                    ArrayList<CommentDB> commentslist = new ArrayList<>();
+                                    fireDB.collection("Comments")
+                                            .whereEqualTo("User_SSN", userid)
+                                            .whereEqualTo("C_Num", data.SSN)
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        Log.d("은하", "여기까지?commenttasksuccess");
+                                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                                            Log.d("은하", document.getId() + " => " + document.getData());
+                                                            CommentDB tempcommentdb = document.toObject(CommentDB.class);
+                                                            commentslist.add(tempcommentdb);
+                                                            Log.d("은하", "commentslist에 잘 추가되었나? "+commentslist);
                                                         }
-                                                    });
-                                        }
-                                    }else{
-                                        Log.d("은하", "get() failed");
-                                    }
+                                                        Log.d("은하", "finalI : "+ data.SSN +", commentslist : "+commentslist);
+                                                        if(commentslist != null) {
+                                                            commentDBS.add(data.SSN, commentslist);
+                                                        }
+                                                        Log.d("은하", "commentDBS : "+commentDBS.toString());
+                                                    } else {
+                                                        Log.d("은하", "Error getting documents: ", task.getException());
+                                                    }
+                                                    //getcommentsfirestore(finalI, userid);
+                                                    Log.d("은하", "여기 recyclerlist: "+recyclerlist);
+                                                    Log.d("은하", "여기?");
+                                                    setrecyclerview(recyclerlist, context);
+
+                                                }
+                                            });
                                 }
-                            });
-
+                            }
+                            else{
+                                Log.d("은하", "Error getting documents: ", task.getException());
+                            }
                         }
+                    });
 
-
-                    }
-                    else{
-                        Log.d("은하", "사용자 아이디에 해당하는 게시물 없음");
-                    }
-                }
-                else{
-                    Log.d("은하", "get() failed");
-                }
-
-            });
         }
 
 
